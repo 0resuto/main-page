7899b45b08b4fe36f6ab5a6aa0e5cd5b62f7835c
0resuto <pilot.ua4@gmail.com>
2026-08-13 18:19:28 +0300

    fix: update next.config.ts remotePatterns to explicitly allow all paths and queries for github avatars to fix 400 Bad Request

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+        search: '',
       },
     ],
   },
